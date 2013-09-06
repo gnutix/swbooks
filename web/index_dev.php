6f8499746612b50c9c58313d6fa5cb8180b4c7cb
Dorian Villet <dorian.villet@liip.ch>
2013-09-07 00:57:19 +0200

    Add multiple frontend controllers and adapt configurations accordingly.

---
+++ web/index_dev.php
@@ -0,0 +1,12 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Gnutix\Application\Kernel;
+
+// Load the PSR-0 autoloader
+$root = __DIR__.'/..';
+require_once $root.'/vendor/autoload.php';
+
+$kernel = new Kernel('dev');
+$response = $kernel->handle(Request::createFromGlobals());
+$response->send();
