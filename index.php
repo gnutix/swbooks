e1a67648b3e791444c366d6caa1189821ee9f627
Dorian Villet <dorian.villet@liip.ch>
2013-09-07 12:44:57 +0200

    Move the books.xml file to ease merging with dev.

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ date_default_timezone_set('Europe/Zurich');
 include('functions.php');
 
 // On récupère le XML
-$xml = getXML('books.xml');
+$xml = getXML('data/books.xml');
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
