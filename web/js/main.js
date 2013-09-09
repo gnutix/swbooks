6bb950f42f5562b822f83323f71dbfa09edf8d0f
Dorian Villet <dorian.villet@liip.ch>
2013-09-09 16:43:31 +0200

    Cleanup auto-rowspan script and removing unneeded loop.

--- web/js/main.js
+++ web/js/main.js
@@ -6,42 +6,38 @@ $(document).ready(function () {
         $(this).attr('colspan', $('thead tr th', $(this).parents('table')).length).removeAttr('data-full-colspan');
     });
 
-    // Loop over the eras headings
-    $('tbody', booksTable).each(function () {
+    // Loop over the books rows
+    $('tbody tr', booksTable).each(function () {
+        var row = $(this);
 
-        // Loop over the next rows that are not era headings
-        $('tr', $(this)).each(function () {
-            var row = $(this);
+        // Then over the columns
+        $('td', row).each(function (columnIndex) {
+            var column = $(this),
+                columnHTML = column.outerHTML();
 
-            // Then over the columns
-            $('td', row).each(function (columnIndex) {
-                var column = $(this),
-                    columnHTML = column.outerHTML();
+            // Set the current rowspan (from the DOM) on the object, or 1 otherwise
+            column.data('rowspan', undefined !== column.attr('rowspan') ? parseInt(column.attr('rowspan')) : 1);
 
-                // Set the current rowspan (from the DOM) on the object, or 1 otherwise
-                column.data('rowspan', undefined !== column.attr('rowspan') ? parseInt(column.attr('rowspan')) : 1);
+            // Loop over the next rows
+            row.nextAll('tr').each(function () {
+                var nextColumn = $(this).find('td').eq(columnIndex);
 
-                // Loop over the next rows
-                row.nextAll('tr').each(function () {
-                    var nextColumn = $(this).find('td').eq(columnIndex);
+                // If the next column is different, there's nothing to do
+                if (nextColumn.outerHTML() !== columnHTML) {
+                    return false;
+                }
 
-                    // If the next column is different, there's nothing to do
-                    if (nextColumn.outerHTML() !== columnHTML) {
-                        return false;
-                    }
+                nextColumn.data('remove', true);
+                column.data('rowspan', column.data('rowspan') + 1);
 
-                    nextColumn.data('remove', 1);
-                    column.data('rowspan', column.data('rowspan') + 1);
-
-                    return true;
-                });
+                return true;
             });
         });
     });
 
     // Once the tagging is done, here comes the real work
     $('tbody tr td', booksTable).each(function () {
-        if (1 == $(this).data('remove')) {
+        if ($(this).data('remove')) {
             $(this).remove();
         } else if (1 < $(this).data('rowspan')) {
             $(this).attr('rowspan', $(this).data('rowspan'));
