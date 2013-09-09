ed3387018360721d0b4ed4dc3f21c4f8f8ede9c5
Dorian Villet <dorian.villet@liip.ch>
2013-09-09 17:13:42 +0200

    [tmp][ci skip] Start using a reverse'd loop to do everything in one loop, on the fly.

--- web/js/main.js
+++ web/js/main.js
@@ -7,35 +7,43 @@ $(document).ready(function () {
     });
 
     // Loop over the books rows
-    $('tbody tr', booksTable).each(function () {
+    $.each($('tbody tr', booksTable).get().reverse(), function () {
         var row = $(this);
 
         // Then over the columns
-        $('td', row).each(function (columnIndex) {
-            var column = $(this),
-                columnHTML = column.outerHTML();
-
-            // Set the current rowspan (from the DOM) on the object, or 1 otherwise
-            column.data('rowspan', undefined !== column.attr('rowspan') ? parseInt(column.attr('rowspan')) : 1);
+        $('td', row).each(function (currentColumnIndex) {
+            var currentColumn = $(this),
+                currentColumnHTML = currentColumn.outerHTML(),
+                nextRows = row.prevAll('tr'),
+                nbNextRows = nextRows.length,
+                column = null,
+                lastColumn = null;
 
             // Loop over the next rows
-            row.nextAll('tr').each(function () {
-                var nextColumn = $(this).find('td').eq(columnIndex);
+            nextRows.each(function (columnIndex) {
+                if (null !== column) {
+                    lastColumn = column;
+                }
+                column = $(this).find('td').eq(currentColumnIndex);
 
                 // If the next column is different, there's nothing to do
-                if (nextColumn.outerHTML() !== columnHTML) {
+                if (column.outerHTML() !== currentColumnHTML) {
+                    if (currentColumn.data('remove')) {
+                        column.attr('rowspan', rowspan + 1);
+                    }
+
                     return false;
                 }
 
-                nextColumn.data('remove', true);
-                column.data('rowspan', column.data('rowspan') + 1);
+                currentColumn.data('remove');
+                column.remove();
 
                 return true;
             });
         });
     });
 
-    // Once the tagging is done, here comes the real work
+    /* Once the tagging is done, here comes the real work
     $('tbody tr td', booksTable).each(function () {
         if ($(this).data('remove')) {
             $(this).remove();
@@ -43,4 +51,5 @@ $(document).ready(function () {
             $(this).attr('rowspan', $(this).data('rowspan'));
         }
     });
+    */
 });
